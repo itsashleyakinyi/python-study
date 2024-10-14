8c0d27d32472098954723952eee7106854f69fb4
itsashleyakinyi <naomiashley75@gmail.com>
2024-10-14 16:58:17 +0300

    first commit

---
+++ areaofcircle.py
@@ -0,0 +1,4 @@
+radius=60
+pi=3.142
+area=pi*radius**2
+print(area)
---
+++ intro.py
@@ -0,0 +1,20 @@
+print("Ashley Naomi")
+# print is used to display the python output on the terminal
+fname="Ashley"
+fname="Naomi"
+print(fname)
+
+# Write a program that adds 2 numbers "num1" and "num2" and stores them in a variable called "sum"
+num1=10
+num2=20
+sum=num1 + num2
+print(sum)
+
+# Declare 2 variables; fname and lname add them up to get full name
+first_name="Ashley"
+last_name="Naomi"
+full_name=first_name +" " +last_name
+print(full_name)
+
+phone_number=input("enter phone number")
+print(phone_number)
